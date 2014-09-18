caeb23c0f5d341113133b7a0a05b5bbe01c5401d
Isabel Suhr <isabels@lclark.edu>
2014-09-18 12:58:28 -0700

    Lab 1 completed

--- lab1/app.rb
+++ lab1/app.rb
@@ -6,19 +6,17 @@ Bundler.require
 
 #change this for your stuff
 get '/' do
-	file_contents = File.read('todo.txt')
-	@lines = file_contents.split("/n")
-	# lines.each do |line|
-	# 	task, due_date = line.split " - "
-	# @items = "#{task} - #{due_date}"#creates the instance variable. somehow put to this??????
+	# file_contents = File.read('todo.txt')
+	@lines = File.readlines('todo.txt')
 	erb :index
 end
 
-# post '/' do
-# 	File.open("todo.txt", "a") do |file|
-#     	file.puts "#{params[:task]} - #{params[:date]}"
-#   	end
-# end
+post '/' do
+	File.open("todo.txt", "a") do |file|
+    	file.puts "#{params[:task]} - #{params[:date]}"
+  	end
+  	redirect '/'
+end
 
 #get '/99bottles' do
 #  @lyrics = (1..99).to_a.reverse.map {|i| "#{i} bottles of beer on the wall, #{i} bottles of beer. Take one down, pass it around, #{i-1} bottles of beer on the wall."}
